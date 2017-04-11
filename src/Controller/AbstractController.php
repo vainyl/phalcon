d153c56b2bf33fe02ea55b764e4d067d1a627c61
allflame <taras.p.gyrnik@gmail.com>
2017-04-12 01:32:55 +0300

    Added missing abstract modificator

--- src/Controller/AbstractController.php
+++ src/Controller/AbstractController.php
@@ -25,7 +25,7 @@ use Psr\Http\Message\ServerRequestInterface;
  * @property ResponseInterface response
  * @property \ArrayAccess cookies
  */
-class AbstractController extends Controller implements ControllerInterface
+abstract class AbstractController extends Controller implements ControllerInterface
 {
     /**
      * @inheritDoc
