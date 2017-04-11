696aa0afd952cfc35e8c463cb1510d6c73c7d081
allflame <taras.p.gyrnik@gmail.com>
2017-04-12 00:43:37 +0300

    Added ControlleInterface

--- src/Controller/AbstractController.php
+++ src/Controller/AbstractController.php
@@ -1,30 +1,53 @@
 <?php
 /**
- * Vain Framework
+ * Vainyl
  *
  * PHP Version 7
  *
- * @package   vain-phalcon
+ * @package   Phalcon
  * @license   https://opensource.org/licenses/MIT MIT License
- * @link      https://github.com/allflame/vain-phalcon
+ * @link      https://vainyl.com
  */
+declare(strict_types=1);
 
 namespace Vainyl\Phalcon\Controller;
 
-use Phalcon\Mvc\Controller as PhalconMvcController;
-use Vain\Core\Http\Cookie\Storage\CookieStorageInterface;
-use Vain\Core\Http\Request\VainServerRequestInterface;
-use Vain\Core\Http\Response\VainResponseInterface;
+use Phalcon\Http\ResponseInterface;
+use Phalcon\Mvc\Controller;
+use Psr\Http\Message\ServerRequestInterface;
 
 /**
  * Class AbstractController
  *
  * @author Taras P. Girnyk <taras.p.gyrnik@gmail.com>
  *
- * @property VainServerRequestInterface $request
- * @property VainResponseInterface      $response
- * @property CookieStorageInterface     $cookies
+ * @property ServerRequestInterface request
+ * @property ResponseInterface response
+ * @property \ArrayAccess cookies
  */
-abstract class AbstractController extends PhalconMvcController
+class AbstractController extends Controller implements ControllerInterface
 {
+    /**
+     * @inheritDoc
+     */
+    public function getRequest(): ServerRequestInterface
+    {
+        return $this->request;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getResponse(): ResponseInterface
+    {
+        return $this->response;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getCookies(): \ArrayAccess
+    {
+        return $this->cookies;
+    }
 }
---
+++ src/Controller/ControllerInterface.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Vainyl
+ *
+ * PHP Version 7
+ *
+ * @package   Phalcon
+ * @license   https://opensource.org/licenses/MIT MIT License
+ * @link      https://vainyl.com
+ */
+declare(strict_types=1);
+
+namespace Vainyl\Phalcon\Controller;
+
+use Phalcon\Http\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+
+/**
+ * Interface ControllerInterface
+ *
+ * @author Taras P. Girnyk <taras.p.gyrnik@gmail.com>
+ */
+interface ControllerInterface
+{
+    /**
+     * @return ServerRequestInterface
+     */
+    public function getRequest() : ServerRequestInterface;
+
+    /**
+     * @return ResponseInterface
+     */
+    public function getResponse() : ResponseInterface;
+
+    /**
+     * @return \ArrayAccess
+     */
+    public function getCookies() : \ArrayAccess;
+}
